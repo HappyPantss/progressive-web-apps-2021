0b94e9553b9511a02012d228f496a91b6b8ffa4a
HappyPantss <sergioeijben@hotmail.com>
2021-03-12 09:41:23 +0100

    :construction: Working on refactoring WAFS app

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // Create a route for our overview page
 app.get('/', function(req, res) {
     res.redirect('/overview');
 });
 
-// Create a home route
+// Create a overview route
 app.get('/overview', function(req, res) {
-    fetch("https://jsonplaceholder.typicode.com/posts/", {
+    fetch("https://ovrstat.com/stats/pc/Sergini-21678", {
         method: "GET",
         headers: { "Content-Type": "application/json" }
     })
@@ -28,13 +28,12 @@ app.get('/overview', function(req, res) {
     .then(res => res.json())
         .then(json =>
             res.render('posts', {
-                title: 'Posts',
                 postData: json
             }))
         .catch(err => console.log(err))
 });
 
-// Create a home route
+// Create a detail route
 app.get('/hero/:id', function(req, res) {
     fetch("https://jsonplaceholder.typicode.com/posts/" + req.params.id, {
         method: "GET",
